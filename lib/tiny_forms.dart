14fe77b1ed795099bebbfa989a4d925730d6e69f
sheeroo <sirogiannisgiw@gmail.com>
2023-09-16 21:29:13 +0300

    ✨ fixed exports

--- lib/tiny_forms.dart
+++ lib/tiny_forms.dart
@@ -2,5 +2,6 @@
 /// and lightweight forms with easy customization and validation.
 library tiny_forms;
 
+export 'src/extensions/text_form_field_extension.dart';
 export 'src/form_client.dart';
 export 'src/tiny_forms.dart';
